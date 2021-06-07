4d328a86f63361f4a0726c89163b47d4e8b8c27a
AutonomousSoup <81836666+AutonomousSoup@users.noreply.github.com>
2021-06-07 13:17:32 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+﻿window.onload = () => {
+     let places = staticLoadPlaces();
+     renderPlaces(places);
+};
+
+function staticLoadPlaces() {
+    return [
+        {
+            name: 'majesticbeasties',
+            location: {
+                lat: 39.8073678,
+                lng: -105.1065006,
+           
+            }
+        },
+    ];
+}
+
+function renderPlaces(places) {
+    let scene = document.querySelector('a-scene');
+
+    places.forEach((place) => {
+        let latitude = place.location.lat;
+        let longitude = place.location.lng;
+
+        let model = document.createElement('a-entity');
+        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+        model.setAttribute('gltf-model', './assets/cuppycake/scene.gltf');
+        model.setAttribute('rotation', '0 180 0');
+        model.setAttribute('animation-mixer', '');
+        model.setAttribute('scale', '0.5 0.5 0.5');
+
+        model.addEventListener('loaded', () => {
+            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+        });
+
+        scene.appendChild(model);
+    });
+}
