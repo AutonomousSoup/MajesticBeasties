8faf2ce8ead65c9bb8585b4db18eca414f85a4a0
AutonomousSoup <81836666+AutonomousSoup@users.noreply.github.com>
2021-06-07 12:57:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ window.onload = () => {
 function staticLoadPlaces() {
     return [
         {
-            name: 'Magnemite',
+            name: 'majesticbeasties',
             location: {
-                lat: 44.496470,
-                lng: 11.320180,
+                lat: 39.8073678,
+                lng: -105.1065006,
+           
             }
         },
     ];
@@ -24,7 +25,7 @@ function renderPlaces(places) {
 
         let model = document.createElement('a-entity');
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
+        model.setAttribute('gltf-model', './assets/cuppycake/scene.gltf');
         model.setAttribute('rotation', '0 180 0');
         model.setAttribute('animation-mixer', '');
         model.setAttribute('scale', '0.5 0.5 0.5');
